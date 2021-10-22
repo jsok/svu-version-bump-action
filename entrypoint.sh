acafe17eb1f9f02dd6b8d4c1219b22a1501e72ea
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2021-10-22 13:01:19 +1100

    Add version-without-prefix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,13 @@ INITIAL=${4:-}
 if svu --pattern="${PATTERN}" --prefix="${PREFIX}" current 2>/dev/null 1>/dev/null
 then
     version="$(svu --pattern="${PATTERN}" --prefix="${PREFIX}" "${BUMP}")"
+    version_without_prefix="$(svu --pattern="${PATTERN}" --prefix="${PREFIX}" --strip-prefix "${BUMP}")"
 else
     if [ -n "${INITIAL}" ]
     then
         echo "::warning Could not find a current version, initialising version to ${INITIAL}"
         version="${PREFIX}${INITIAL}"
+        version_without_prefix="${INITIAL}"
     else
         echo "::error Could not find a current version!"
         exit 1
@@ -25,3 +27,4 @@ echo "${version}"
 echo "::endgroup::"
 
 echo "::set-output name=version::${version}"
+echo "::set-output name=version-without-prefix::${version_without_prefix}"
